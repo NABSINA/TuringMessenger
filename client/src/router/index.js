a39612ca6c709684dfe3ee131bb1e6abe31d2567
Angad Singh <Angad@Angads-Macbook-Pro.local>
2020-11-07 09:37:44 -0600

    Added MessageBot view

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -14,6 +14,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Matchmaking.vue')
+  },
+  {
+  path: '/message-bot',
+  name: 'MessageBot',
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  component: () => import(/* webpackChunkName: "about" */ '../views/MessageBot.vue')
   }
 ]
 
---
+++ client/src/views/MessageBot.vue
@@ -0,0 +1,35 @@
+<template>
+    <header class="header">
+    <div>
+        <div class="message-bot">
+            <h1>Message Bot</h1>
+        </div>
+    </div>
+    </header>
+</template>
+
+<script>
+import * as api from '../services/api';
+
+export default {
+    name: "MessageBot",
+    
+    messages(){
+       
+    }
+}
+</script>
+
+<style scoped>
+    .header {
+        background: #333;
+        color: #fff;
+        text-align: center;
+        padding: 10px;
+    }
+
+    .header a {
+        color: #fff;
+        padding-right: 5px;
+    }
+</style>
