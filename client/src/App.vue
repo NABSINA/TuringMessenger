a01ddc7ba076ea0972bd1e3a6a1c270d861040d7
Angad Singh <Angad@Angads-Macbook-Pro.local>
2020-11-07 01:51:02 -0600

    First commit

---
+++ client/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+   <div id="app">
+     <Header />
+        
+   </div>
+</template>
+
+<script>
+
+import Header from './components/layout/Header';
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+</style>
