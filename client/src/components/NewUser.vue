d715abf16f7a0150a7cc575a975b9e7f49134a05
Angad Singh <Angad@Angads-Macbook-Pro.local>
2020-11-07 02:32:12 -0600

    Added NewUser component

---
+++ client/src/components/NewUser.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <form>
+            <input type="text" name="username" placeholder="New Username...">
+            <input type="submit" value="Go" class="btn">
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "NewUser"
+}
+</script>
+
+<style scoped>
+    form {
+        display: flex;
+    }
+
+    input[type="text"]{
+
+        flex: 10;
+        padding: 15px;
+    }
+
+    input[type="submit"]{
+        flex: 2;
+    }
+</style>
